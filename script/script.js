0eab831c6ae792f1e6c9dff147e5a47128d300bb
Miguel Munhoz <m.munhoz@aluno.senai.br>
2026-05-19 14:38:00 -0300

    Exemplo

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 class Foto {
-    constructor(url, legenda, lat, lon) {
+    constructor(url, legenda, descricao, lat, lon) {
         this.url = url;
         this.legenda = legenda;
+        this.descricao = descricao;
         this.localizacao = { lat, lon };
         this.dataCriacao = new Date();
     }
@@ -9,32 +10,38 @@ class Foto {
 
 const btn = document.querySelector("#btn-adicionar");
 const galeria = document.querySelector("#galeria");
-console.log(btn);
-
 
 btn.addEventListener('click', () => {
     const url = document.querySelector('#ipt-url').value;
     const legenda = document.querySelector('#ipt-title').value;
+    const descricao = document.querySelector('#ipt-desc').value;
     const lat = parseFloat(document.querySelector('#ipt-lat').value) || 0;
     const lon = parseFloat(document.querySelector('#ipt-lon').value) || 0;
 
-    if (!url || !legenda) return alert("Por favor, insira a URL e a legenda da foto.");
+    if (!url || !legenda) {
+        return alert("Por favor, insira a URL e a legenda da foto.");
+    }
 
-    const novaFoto = new Foto(url, legenda, lat, lon);
+    const novaFoto = new Foto(url, legenda, descricao, lat, lon);
 
     const card = document.createElement('div');
     card.className = 'card-foto';
+
     card.innerHTML = `
         <img src="${novaFoto.url}" alt="${novaFoto.legenda}">
-        <div class="card.info">
+        
+        <div class="card-info">
             <h3>${novaFoto.legenda}</h3>
-            <p>${novaFoto.resumo}</p>
+            <p>${novaFoto.descricao}</p>
+
             <div>
-                🎈${novaFoto.localizacao}
+                🎈 Lat: ${novaFoto.localizacao.lat}, Lon: ${novaFoto.localizacao.lon}
             </div>
-         </div> 
- `;
+        </div>
+    `;
+
     galeria.prepend(card);
-    // Limpar forms
+
+    // limpar inputs
     document.querySelectorAll('input').forEach(i => i.value = "");
 });
