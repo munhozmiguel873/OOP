da0aefc875f26b6038e2153c7037acbdb4b74e6c
Miguel Munhoz <m.munhoz@aluno.senai.br>
2026-05-19 15:45:56 -0300

    Exemplo

--- script/script.js
+++ script/script.js
@@ -8,6 +8,20 @@ class Foto {
     }
 }
 
+function pegarLocalizacao() {
+    navigator.geolocation.getCurrentPosition(
+        (posicao) => {
+            document.querySelector("#ipt-lat").value = posicao.coords.latitude;
+            document.querySelector("#ipt-lon").value = posicao.coords.longitude;
+
+            alert("Localização capturada com sucesso 📍");
+        },
+        () => {
+            alert("Permissão negada ou erro ao capturar localização.");
+        }
+    );
+}
+
 const btn = document.querySelector("#btn-adicionar");
 const galeria = document.querySelector("#galeria");
 
@@ -44,9 +58,10 @@ btn.addEventListener('click', () => {
                 🕒 ${novaFoto.dataCriacao.toLocaleString()}
             </div>
 
-            <div class="btn-local" id="btn-local">
-                📍 Usar minha localização</div>            
-            </div>
+            <button class="btn-local" onclick="pegarLocalizacao()">
+                📍 Usar minha localização
+            </button>
+        </div>
     `;
 
     galeria.prepend(card);
